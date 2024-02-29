8c88a90c26fdd7330405e06edb0f981bf765b4de
szabson33 <piotrszabram33@gmail.com>
2024-02-29 15:39:32 +0100

    c

--- php_commands/setvolume.php
+++ php_commands/setvolume.php
@@ -3,7 +3,7 @@ $volume = $_GET['volume'];
 $comand = 'amixer -c 0 sset Digital '.strval($volume);
 $output=null;
 $retvalue=null;
-// exec($comand,$output,$retvalue);
+exec($comand,$output,$retvalue);
 
 echo $volume;
 ?>
